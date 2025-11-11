88deca92dc85f3132535411252aab9840a46a2b7
lukasztaraszka <l.taraszka@statscore.com>
2025-11-11 23:25:22 +0100

    Update version to 0.0.7; remove Polish translation constants import from TelegramBot and TelegramBotScheduler modules.

--- TelegramBotScheduler/main.py
+++ TelegramBotScheduler/main.py
@@ -9,8 +9,6 @@ from config import Config
 from SqlDB.database import init_db
 from Modules.Scheduler.scheduler import SchedulerService
 
-import Modules.TranslationTools.pl.constants
-
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
